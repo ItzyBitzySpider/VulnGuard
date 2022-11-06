af7803815b6518a64e3a3ef44a7190b04d44d1ea
Sean - <sean.seah0311@gmail.com>
2022-11-07 00:30:20 +0800

    refactor regexScan and tested regexScan

--- src/scanner.js
+++ src/scanner.js
@@ -33,20 +33,52 @@ async function semgrepScan(configs, path) {
 
 async function regexScan(rules, path) {
     var hits = [];
-    for (const rule of rules) { //Note: Regex is applied line-by-line as opposed to Semgrep, which is applied file-by-file
-        var line_no = 0;
-        var rd = readline.createInterface({ input: fs.createReadStream(path), console: false });
-        for await (const line of rd) {
-            line_no += 1;
-            if (rule.regex instanceof RegExp) { //Can provide start and end indices since this is a simple regex rule (not a regex tree)
-                let match;
-                while ((match = rule.regex.exec(line)) !== null) {
-                    hits.push({ "id": rule.id, "line_no": line_no, "start": match.index, "end": rule.regex.lastIndex });
-                }
-            } else {
-                if (applyRegexCheck(rule.regex, "regex_and", line)) {
-                    hits.push({ "id": rule.id, "line_no": line_no });
-                }
+    //array of promises to run regex for each rule
+    const promises = rules.map((rule) => { return regexRuleScan(rule, path); });
+    const results = await Promise.all(promises);
+    for (const result of results) {
+        hits = hits.concat(result);
+    }
+    console.log(hits);
+    return hits;
+}
+
+async function regexRuleScan(rule, path) {
+    var hits = [];
+    var line_no = 0;
+    var rd = readline.createInterface({ input: fs.createReadStream(path), console: false });
+    for await (const line of rd) {
+        line_no += 1;
+        if (rule.regex instanceof RegExp) { //Can provide start and end indices since this is a simple regex rule (not a regex tree)
+            let match;
+            while ((match = rule.regex.exec(line)) !== null) {
+                hits.push({
+                    "severity": rule.severity,
+                    "range": {
+                        "start": match.index,
+                        "end": rule.regex.lastIndex,
+                    },
+                    "line_no": line_no,
+                    "message": rule.message,
+                    "fix": rule.fix ?? null,
+                    "id": rule.id,
+                    "source": "VulnGuard",
+                });
+            }
+        } else {
+            if (applyRegexCheck(rule.regex, "regex_and", line)) {
+                hits.push({
+                    "severity": rule.severity,
+                    "range": {
+                        "start": null,
+                        "end": null,
+                    },
+                    "line_no": line_no,
+                    "message": rule.message,
+                    "fix": rule.fix ?? null,
+                    "id": rule.id,
+                    "source": "VulnGuard",
+                });
             }
         }
     }
@@ -60,109 +92,109 @@ function scan(path) {
 }
 
 function applyRegexCheck(node, parent_type, text) { //Assumes tree is valid
-    var res
+    var res;
     if (parent_type === "regex" || parent_type === "regex_and") {
-        res = true //Neutral element of AND is 1
+        res = true; //Neutral element of AND is 1
         for (const field of node) {
-            const propertyNames = Object.getOwnPropertyNames(field)
-            const key = propertyNames[0]
-            const val = field[key]
+            const propertyNames = Object.getOwnPropertyNames(field);
+            const key = propertyNames[0];
+            const val = field[key];
 
             if (val instanceof RegExp) {
                 if (key === "regex_not") {
-                    res &= !(val.test(text))
+                    res &= !(val.test(text));
                 } else {
-                    res &= val.test(text)
+                    res &= val.test(text);
                 }
             } else {
-                res &= applyRegexCheck(val, key, text)
+                res &= applyRegexCheck(val, key, text);
             }
         }
     } else { //regex_or
-        res = false //Neutral element of OR is 0
+        res = false; //Neutral element of OR is 0
         for (const field of node) {
-            const propertyNames = Object.getOwnPropertyNames(field)
-            const key = propertyNames[0]
-            const val = field[key]
+            const propertyNames = Object.getOwnPropertyNames(field);
+            const key = propertyNames[0];
+            const val = field[key];
 
             if (val instanceof RegExp) {
                 if (key === "regex_not") {
-                    res |= !(val.test(text))
+                    res |= !(val.test(text));
                 } else {
-                    res |= val.test(text)
+                    res |= val.test(text);
                 }
             } else {
-                res |= applyRegexCheck(val, key, text)
+                res |= applyRegexCheck(val, key, text);
             }
         }
     }
-    return res
+    return res;
 }
 
-var semgrepRules = []
-var regexRules = []
+var semgrepRules = [];
+var regexRules = [];
 
 function validateRegexTree(node) {
     for (const field of node) {
-        const propertyNames = Object.getOwnPropertyNames(field)
+        const propertyNames = Object.getOwnPropertyNames(field);
         if (propertyNames.length !== 1) {
-            throw "Expected 1 property name, got " + propertyNames.length + " (" + propertyNames + ")"
+            throw "Expected 1 property name, got " + propertyNames.length + " (" + propertyNames + ")";
         }
 
-        const key = propertyNames[0]
+        const key = propertyNames[0];
         if (key !== "regex" && key !== "regex_and" && key !== "regex_or" && key !== "regex_not") {
-            throw "Unknown key name " + key
+            throw "Unknown key name " + key;
         }
 
-        const val = field[key]
-        const val_type = typeof val //TODO: Should val_type be strictly enforced?
+        const val = field[key];
+        const val_type = typeof val; //TODO: Should val_type be strictly enforced?
         /*if(val_type !== "string" && val_type !== "object"){
             throw "Unknown value type " + val_type
         }*/
 
         if (val_type === "object") {
             if (key === "regex_not") {
-                throw "regex_not cannot be used on a regex subtree"
+                throw "regex_not cannot be used on a regex subtree";
             }
-            validateRegexTree(val)
+            validateRegexTree(val);
         } else {
             if (key === "regex_or") {
-                throw "regex_or can only be used on a regex subtree"
+                throw "regex_or can only be used on a regex subtree";
             }
-            field[key] = new RegExp(field[key], 'g') //Compile regex while validating tree
+            field[key] = new RegExp(field[key], 'g'); //Compile regex while validating tree
         }
     }
 }
 
 function loadRegexRules(path) {
-    const cfg = fs.readFileSync(path, 'utf8')
-    const dat = yaml.parse(cfg)
+    const cfg = fs.readFileSync(path, 'utf8');
+    const dat = yaml.parse(cfg);
     for (const rule of dat.rules) { //TODO: Implement fix for Regex?
         if (!('id' in rule)) {
-            throw "rule is missing 'id' field"
+            throw "rule is missing 'id' field";
         }
         if (!('message' in rule)) {
-            throw "rule is missing 'message' field"
+            throw "rule is missing 'message' field";
         }
         if (!('severity' in rule)) {
-            throw "rule is missing 'severity' field"
+            throw "rule is missing 'severity' field";
         }
         if (!('regex' in rule)) {
-            throw "rule is missing 'regex' field"
+            throw "rule is missing 'regex' field";
         } //TODO: Throw error on unrecognized fields (fix, metadata optional)
 
         if (typeof rule.regex === "object") {
-            validateRegexTree(rule.regex)
+            validateRegexTree(rule.regex);
         } else { //TODO: Should val_type be strictly enforced?
-            rule.regex = new RegExp(rule.regex, 'g') //Compile regex while validating
+            rule.regex = new RegExp(rule.regex, 'g') ;//Compile regex while validating
         }
-        regexRules.push(rule)
+        regexRules.push(rule);
     }
     //console.log(util.inspect(regexRules, false, null, true))
 }
 
 function loadSemgrepRules(path) { //TODO: Do proper data validation?
-    semgrepRules.push(path)
+    semgrepRules.push(path);
 }
 
 loadRegexRules('rules.yml')
